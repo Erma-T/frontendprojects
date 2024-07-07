96fc2d5abbedf388ae48cc72acbd0ba67d9855ec
erma-T <ermy143@gmail.com>
2024-07-07 22:44:03 +0300

    final static projects card

---
+++ src/lib/data.ts
@@ -0,0 +1,21 @@
+export const projectsData = [
+    {
+      title: "About Me:",
+      description:
+        "I am Ermiyas Tilahun and I turn ideas into reality. Full stack developer with a passion for clean code and user-friendly experiences. Let's build something magical.",
+      link: "https://ermiyas.com",
+    },
+    {
+      title: "About This Project",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+      link: "https://github.com/Erma-T/30daysFrontEndBasic",
+    },
+    {
+      title: "More Projects",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "https://github.com/Erma-T?tab=repositories",
+    },
+    
+  ];
