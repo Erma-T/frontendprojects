8101b2588387935ba08c804bb473a8fb4c1050c1
erma-T <ermy143@gmail.com>
2024-07-08 16:35:29 +0300

    Link fixed for quize for netlify deployment error

--- src/app/mainsections/projectsCard.tsx
+++ src/app/mainsections/projectsCard.tsx
@@ -72,6 +72,7 @@ const ProjectsCard = ({
                             <div className="flex justify-end space-x-2 gap-2">
                                 <Link
                                     href={item?.livelink}
+                                    key={item?.livelink}
                                     className="flex px-[1rem] bg-[#010B0E] justify-center py-1 justify-items-center rounded-lg"  
                                     target="_blank"
                                     >
@@ -84,6 +85,7 @@ const ProjectsCard = ({
 
                                 <Link
                                     href={item?.repolink}
+                                    key={item?.repolink}
                                     className="flex px-[1rem] bg-[#010B0E] justify-center py-1 justify-items-center rounded-lg" 
                                     target="_blank">
                                     
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,8 +19,8 @@ export const projectsData = [
         imageurl: "/images/show2.webp",
         title: "Quiz App",
         description: "A basic quiz app that displays questions with their respective four options to select the correct one. If correct, will mark it in green if not, in red and show total score.",
-        livelink: "https://basicfrontend.netlify.app/day%202%20todo%20app/",
-        repolink: "https://github.com/Erma-T/30daysFrontEndBasic/tree/main/Day%202%20TODO%20App",
+        livelink: "https://basicfrontend.netlify.app/day%203%20quiz%20app/",
+        repolink: "https://github.com/Erma-T/30daysFrontEndBasic/tree/main/Day%203%20Quiz%20App",
     },
     
 
